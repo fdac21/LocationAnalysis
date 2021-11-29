76c447ce8b05e25d6314c067b002d4c7b85c82b2
Joey Lemon <joeyclemon@gmail.com>
2021-11-29 10:52:13 -0500

    update location analysis

--- src/services/LocationService.js
+++ src/services/LocationService.js
@@ -3,34 +3,56 @@ import haversine from 'haversine'
 
 const daysOfWeek = { 0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat' }
 
+/**
+ * Get the number of states the user has visited
+ * @returns {Number} The number of states visited
+ */
 export function getStatesVisited () {
     // Placeholder for calculation
     return 5
 }
 
+/**
+ * Get the number of miles traveled using the given list of location pings
+ * @param {Array<Object>} locs An array of location pings
+ * @returns {Number} The number of miles traveled between the given locations
+ */
 function getMilesTraveled (locs) {
+    if (locs.length === 0) { return 0 }
+
     let sum = 0
-
-    if (locs.length === 0) { return sum }
-
     let prevLoc = locs[0]
-    for (const loc of locs.slice(1)) {
+    for (let i = 10; i < locs.length; i += 10) {
+        const loc = locs[i]
         const dist = haversine(prevLoc, loc, { unit: 'mile' })
 
-        // Only sum distances under 2 miles. Distances greater than this are
-        // usually an error within the location tracking software
-        if (dist < 5) { sum += dist }
+        // If the distance between this ping and 10 pings ago is less than 0.5,
+        // it's safe to say the user didn't move
+        if (dist > 0.5) { sum += dist }
 
         prevLoc = loc
     }
 
+    // Account for the last location in the array since it might be skipped
+    // using the for-loop counter of i += 10
+    const dist = haversine(prevLoc, locs.at(-1), { unit: 'mile' })
+    if (dist > 0.5) { sum += dist }
+
     return Math.round(sum)
 }
 
+/**
+ * Get the total number of miles the user has traveled using all of the location history
+ * @returns {Number} The total number of miles the user has traveled
+ */
 export function getTotalMilesTraveled () {
     return getMilesTraveled(locations).toLocaleString()
 }
 
+/**
+ * Get the number of miles traveled for each day of the past week
+ * @returns {Array<Object>} The miles traveled on each day of the week
+ */
 export function getMilesTraveledPastWeek () {
     // hard code new Date(2021, 10, 10) for now since locations are saved locally instead of from API
     const weekLocations = locations.filter(l => new Date(2021, 10, 29).getTime() - l.date < 604800000)
@@ -48,6 +70,10 @@ export function getMilesTraveledPastWeek () {
     return result
 }
 
+/**
+ * Get the number of miles of the user's longest trip
+ * @returns {String} The number of miles formatted with commas
+ */
 export function getLongestTrip () {
     let longestTrip = 0
     let currentTrip = 0
@@ -57,13 +83,10 @@ export function getLongestTrip () {
         const loc = locations[i]
         const dist = haversine(prevLoc, loc, { unit: 'mile' })
 
-        if (dist > 5) {
+        if (dist > 1.5 && dist < 100) {
             currentTrip += dist
         } else {
-            if (currentTrip < 3000) {
-                longestTrip = Math.max(longestTrip, currentTrip)
-            }
-
+            longestTrip = Math.max(longestTrip, currentTrip)
             currentTrip = 0
         }
 
@@ -73,6 +96,10 @@ export function getLongestTrip () {
     return Math.round(longestTrip).toLocaleString() + 'mi'
 }
 
+/**
+ * Get the average number of miles of all of the user's trips
+ * @returns {String} The number of miles formatted with commas
+ */
 export function getAverageTrip () {
     const totalTrips = { sum: 0, trips: 0 }
     let currentTrip = 0
@@ -82,13 +109,11 @@ export function getAverageTrip () {
         const loc = locations[i]
         const dist = haversine(prevLoc, loc, { unit: 'mile' })
 
-        if (dist > 5) {
+        if (dist > 1.5 && dist < 100) {
             currentTrip += dist
         } else {
-            if (currentTrip < 500) {
-                totalTrips.sum += currentTrip
-                totalTrips.trips += 1
-            }
+            totalTrips.sum += currentTrip
+            totalTrips.trips += 1
 
             currentTrip = 0
         }
